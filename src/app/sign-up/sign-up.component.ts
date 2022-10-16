7fb14d57bcad216373933c1952c27e09837db0ba
kishore951 <kishoresakthivel951@gmail.com>
2022-10-16 20:19:28 +0530

    five

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -8,6 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class SignUpComponent implements OnInit {
 
   constructor() { }
+status:boolean=false
+  name=""
+  phoneNo=""
+  gender=""
+  address=""
+  pincode=""
+  dob=""
+  password=""
+  confirmPassword=""
+
+
+  readValues=()=>{
+    let data ={
+      "name":this.name,
+  "phoneNo":this.phoneNo,
+  "gender":this.gender,
+  "address":this.address,
+  "pincode":this.pincode,
+  "dob":this.dob,
+  "password":this.password,
+  "confirmPassword":this.confirmPassword
+
+ 
+
+    }
+    console.log(data)
+    this.status=true
+  }
+
 
   ngOnInit(): void {
   }
